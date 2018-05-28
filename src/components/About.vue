4c2bc2307256796ea3e9f962fdcbee7bee99d26e
bumjoon kim <>
2018-05-28 17:52:30 +0900

    modal added

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,24 @@
 <template>
-  <p>about about hohihihoihio</p>
+  <div>
+    <p>about about hohihihoihio</p>
+    <button>테스트</button>
+    {{customData}}
+    <CustomInput v-on:addNewItem="addItem"></CustomInput>
+    <CustomInput @addNewItem="addItem"></CustomInput>
+    <Modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">얼라려</h3>
+      <span slot="body" @click="showModal = false">
+        {{customData}}
+        <i class="closeModalBtn fa fa-times" aria-hidden="True"></i>
+      </span>
+    </Modal>
+  </div>
 </template>
 
 <script>
+import CustomInput from './CustomInput.vue'
+import Modal from './Modal.vue'
+
 export default {
   name: 'about',
   beforeRouteEnter (to, from, next) {
@@ -14,13 +30,25 @@ export default {
   beforeRouteLeave (to, from, next) {
     next()
   },
+  components: {
+    'CustomInput': CustomInput,
+    'Modal': Modal
+  },
   data () {
     return {
+      showModal: false,
+      customData: ''
     }
   },
   created () {
     // router 전달 받을때 param 받아서 사용가능
     console.log(this.$route.params)
+  },
+  methods: {
+    addItem (item, aaa) {
+      this.customData = item
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
---
+++ src/components/CustomInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="inner">
+    <input type="text" v-model="newItem">
+    <button v-on:click="addItem">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomInput',
+  data () {
+    return {
+      newItem: ''
+    }
+  },
+  methods: {
+    addItem () {
+      this.$emit('addNewItem', this.newItem, 'okok')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
+
